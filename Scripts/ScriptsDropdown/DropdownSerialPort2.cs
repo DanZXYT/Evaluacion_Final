f1d8872750985b7d89561ed095aa6e284c8b6b52
DanZXYT <83980917+DanZXYT@users.noreply.github.com>
2021-11-07 13:15:47 -0500

    Create DropdownSerialPort2.cs

---
+++ Scripts/ScriptsDropdown/DropdownSerialPort2.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.IO.Ports;
+public class DropdownSerialPort2 : MonoBehaviour
+{
+    public static string PUERTO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dropdown = transform.GetComponent<Dropdown>();
+
+        dropdown.options.Clear();
+
+        List<string> items = new List<string>();
+
+        // listamos los puertos COM
+        string[] ports = SerialPort.GetPortNames();
+        foreach (string COM in ports)
+        {
+            items.Add(COM);
+        }
+
+        foreach (var item in items)
+        {
+            dropdown.options.Add(new Dropdown.OptionData() { text = item });
+        }
+
+        DropdownItemSelected(dropdown);
+
+        dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
+    }
+
+    void DropdownItemSelected(Dropdown dropdown)
+    {
+        int index = dropdown.value;
+
+        PUERTO = dropdown.options[index].text;
+    }
+
+    public string PuertoSelect()
+    {
+        return PUERTO;
+    }
+}
