697f6e93b7887ee017ae9491724c92c930d3d446
DanZXYT <83980917+DanZXYT@users.noreply.github.com>
2021-11-07 13:22:14 -0500

    Create OpenBinario.cs

---
+++ Scripts/ScriptButton/OpenBinario.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class OpenBinario : MonoBehaviour
+{
+    public string portName;
+    public int baudRate;
+    public int reconnectionDelay = 1000;
+    public int maxUnreadMessages = 1;
+    public GameObject messageListener;
+
+    protected Thread thread;
+    protected SerialThreadLinesBIN serialThread;
+    
+
+    public const string SERIAL_DEVICE_CONNECTED = "__Connected__";
+    public const string SERIAL_DEVICE_DISCONNECTED = "__Disconnected__";
+
+    public void OpenSerialBINARIO()
+    {
+        portName = FindObjectOfType<DropdownSerialPort2>().PuertoSelect();
+        baudRate = int.Parse(FindObjectOfType<DropdownSpeed2>().SpeedSelect());
+
+        Debug.Log("port2 = "+portName);
+        Debug.Log("baud2 = "+baudRate);
+
+        serialThread = new SerialThreadLinesBIN(portName,
+                                             baudRate,
+                                             reconnectionDelay,
+                                             maxUnreadMessages);
+        thread = new Thread(new ThreadStart(serialThread.RunForever));
+        thread.Start();
+
+    }
+
+    void OnDisable()
+    {
+        // If there is a user-defined tear-down function, execute it before
+        // closing the underlying COM port.
+        if (userDefinedTearDownFunction != null)
+            userDefinedTearDownFunction();
+
+        // The serialThread reference should never be null at this point,
+        // unless an Exception happened in the OnEnable(), in which case I've
+        // no idea what face Unity will make.
+        if (serialThread != null)
+        {
+            serialThread.RequestStop();
+            serialThread = null;
+        }
+
+        // This reference shouldn't be null at this point anyway.
+        if (thread != null)
+        {
+            thread.Join();
+            thread = null;
+        }
+
+
+    }
+
+    void Update()
+    {
+        // If the user prefers to poll the messages instead of receiving them
+        // via SendMessage, then the message listener should be null.
+        if (messageListener == null)
+            return;
+
+        // Read the next message from the queue
+        string message = (string)serialThread.ReadMessage();
+        if (message == null)
+            return;
+
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(message, SERIAL_DEVICE_CONNECTED))
+            messageListener.SendMessage("OnConnectionEvent", true);
+        else if (ReferenceEquals(message, SERIAL_DEVICE_DISCONNECTED))
+            messageListener.SendMessage("OnConnectionEvent", false);
+        else
+            messageListener.SendMessage("OnMessageArrived", message);
+    }
+
+
+    public string ReadSerialMessage()
+    {
+        // Read the next message from the queue
+        return (string)serialThread.ReadMessage();
+    }
+
+    // ------------------------------------------------------------------------
+    // Puts a message in the outgoing queue. The thread object will send the
+    // message to the serial device when it considers it's appropriate.
+    // ------------------------------------------------------------------------
+    public void SendSerialMessage(string message)
+    {
+        serialThread.SendMessage(message);
+    }
+
+    // ------------------------------------------------------------------------
+    // Executes a user-defined function before Unity closes the COM port, so
+    // the user can send some tear-down message to the hardware reliably.
+    // ------------------------------------------------------------------------
+    public delegate void TearDownFunction();
+    private TearDownFunction userDefinedTearDownFunction;
+    public void SetTearDownFunction(TearDownFunction userFunction)
+    {
+        this.userDefinedTearDownFunction = userFunction;
+    }
+}
